81d361612eeb4766a93a656af1d89b28bae0b611
Ichiro Yamazaki <16699009+ichiroyamazaki@users.noreply.github.com>
2024-12-23 00:19:52 +0800

    Create TaskPerformance.java

---
+++ TaskPerformance.java
@@ -0,0 +1,38 @@
+*
+//This is a simple Java program. And this message is currently hidden and it can't easily find this message.
+//Here is the profile of this creator of this Java Program:
+Name: Ichiro Yamazaki
+Student No.: xxxxxxxxx
+ */
+public class TaskPerformance{
+public static void main(String args[]){
+System.out.println("             ________________________________________________");
+System.out.println("            |                                                |");
+System.out.println("           |    _________________________________________     |");
+System.out.println("           |   |                                         |    |");
+System.out.println("           |   |                                         |    |");
+System.out.println("           |   |                                         |    |");
+System.out.println("           |   |             Welcome to the              |    |");
+System.out.println("           |   |          NetBeans IDE 15 Java           |    |");
+System.out.println("           |   |                                         |    |");
+System.out.println("           |   |         This Java was Made by:          |    |");
+System.out.println("           |   |            Ichiro Yamazaki              |    |");
+System.out.println("           |   |                                         |    |");
+System.out.println("           |   |                                         |    |");
+System.out.println("           |   |                                         |    |");
+System.out.println("           |   |                                         |    |");
+System.out.println("           |   |          STI College Balagtas           |    |");
+System.out.println("           |   |_________________________________________|    |");
+System.out.println("           |                                                  |");
+System.out.println("            |________________________________________________|");
+System.out.println("                   |___________________________________|");
+System.out.println("                ___________________________________________");
+System.out.println("             _-'    .-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.  --- `-_");
+System.out.println("          _-'.-.-. .---.-.-.-.-.-.-.-.-.-.-.-.-.-.-.--.  .-.-.`-_");
+System.out.println("       _-'.-.-.-. .---.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-`__`. .-.-.-.`-_");
+System.out.println("    _-'.-.-.-.-. .-----.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-----. .-.-.-.-.`-_");
+System.out.println(" _-'.-.-.-.-.-. .---.-. .-------------------------. .-.---. .---.-.-.-.`-_");
+System.out.println(":-------------------------------------------------------------------------:");
+System.out.println("`-------------------------------------------------------------------------'");
+    }
+}
